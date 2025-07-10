f660015a0b3e466ab13135c2f830f00fb6dbe5b9
Jitesh <jiteshgopale26@gmail.com>
2025-07-10 12:47:41 +0530

    add image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  images: {
+    unoptimized: true,
+  },
   ignoreDuringBuilds: true, //disable ESLint in production build
 };
 
